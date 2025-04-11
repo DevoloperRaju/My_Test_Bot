42b74e249baa3e654f2bffd2b3b3ebc6b9bbce1a
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-11 18:40:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
 
 const token = '6114573552:AAFUs_xgteTw6vyMj0qK-sc4RwYC_grD37k';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
+// Dynamic command loading
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-  const options = {
-    reply_markup: {
-      keyboard: [
-        ['Hi', 'DJ'], // ১ম লাইনে দুইটা বাটন
-        ['Help'] // ২য় লাইনে একটা বাটন
-      ],
-      resize_keyboard: true, // মোবাইলে সুন্দর করে দেখাবে
-      one_time_keyboard: false // বারবার দেখাবে (true করলে একবার ক্লিক করলে keyboard চলে যাবে)
-    }
-  };
-
-  bot.sendMessage(chatId, 'Choose an option:', options);
-});
-
-// বাটনের message ধরার জন্য
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
-
-  if (text === 'Hi') {
-    bot.sendMessage(chatId, 'Hi!');
-  } else if (text === 'DJ') {
-    bot.sendMessage(chatId, 'I am Ready');
-  } else if (text === 'Help') {
-    bot.sendMessage(chatId, 'How can I help you?');
-  }
-});
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  command(bot);
+}
