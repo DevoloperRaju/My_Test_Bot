a80d171e8be0f0d274c3b3fe4ab09c429a2fed95
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-11 17:58:53 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// আপনার Bot Token এখানে বসান
+const token = '6114573552:AAFUs_xgteTw6vyMj0qK-sc4RwYC_grD37k';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Hi!");
+});
