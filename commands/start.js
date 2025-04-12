1aaaf7917c551e749287f6d58373c7f232ef7c7f
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-12 23:09:54 +0600

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,5 +1,13 @@
-module.exports = (bot) => {
-  bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Hi from start.js!");
+//const mainMenu = require('./main_menu');
+
+//module.exports = (bot) => {
+  //bot.onText(/\/start/, (msg) => {
+ //   const chatId = msg.chat.id;
+
+    // Step 1: Welcome Message
+    bot.sendMessage(chatId, '👋 Welcome to IoLent!\n\nEarn diamonds and grow your income!');
+
+    // Step 2: Show Main Menu
+    mainMenu(bot, chatId);
   });
 };
