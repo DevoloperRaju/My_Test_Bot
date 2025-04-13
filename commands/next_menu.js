0e826c3dea3033862c965cc1bf12f45c0a45d780
DevoloperRaju <106170865+DevoloperRaju@users.noreply.github.com>
2025-04-13 13:28:21 +0600

    Create next_menu.js

---
+++ commands/next_menu.js
@@ -0,0 +1,26 @@
+const mainMenu = require('./main_menu');
+
+module.exports = (bot, chatId) => {
+  const nextMenuOptions = {
+    reply_markup: {
+      keyboard: [
+        ['🎁 Gift Code'],
+        ['🕑 History'],
+        ['📢 Notice'],
+        ['🧩 Learn About IoLent'],
+        ['🏅 LeaderBoard'],
+        ['⬅️ Previous Page']
+      ],
+      resize_keyboard: true
+    }
+  };
+
+  bot.sendMessage(chatId, '📖 Next Page:\nPlease select an option below:', nextMenuOptions);
+
+  // Listen for Previous Page button
+  bot.once('message', (msg) => {
+    if (msg.text === '⬅️ Previous Page') {
+      mainMenu(bot, msg.chat.id);
+    }
+  });
+};
